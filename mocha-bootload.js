beb876cabf926603e644abfd568fe36eb39e23b3
Nathan Tran <ntran013@gmail.com>
2015-08-24 00:04:35 +1000

    remove unused file

--- mocha-bootload.js
+++
@@ -1,7 +0,0 @@
-require('babel/register');
-
-process.on('unhandledRejection', function handlePromise(error) {
-  /* eslint no-console: 0 */
-  console.error('Unhandled Promise Rejection:');
-  console.error(error && error.stack || error);
-});
